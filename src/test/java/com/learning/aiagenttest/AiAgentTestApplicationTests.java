609f8c05737f76d52d4eb0f364d4122cdcd7aeb6
黄映武 <1198795115@qq.com>
2026-03-10 23:57:56 +0800

    first commit

---
+++ src/test/java/com/learning/aiagenttest/AiAgentTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.aiagenttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiAgentTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
