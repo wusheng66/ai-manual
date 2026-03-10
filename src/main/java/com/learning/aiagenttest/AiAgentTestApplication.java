bf313c0cd20101cd0537b334d4e3f831c33a40af
黄映武 <1198795115@qq.com>
2026-03-10 23:57:56 +0800

    first commit

---
+++ src/main/java/com/learning/aiagenttest/AiAgentTestApplication.java
@@ -0,0 +1,13 @@
+package com.learning.aiagenttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiAgentTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiAgentTestApplication.class, args);
+    }
+
+}
