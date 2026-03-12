a59e1a2af306f8467ef046d63a6cb092074cb2d3
黄映武 <1198795115@qq.com>
2026-03-12 23:49:56 +0800

    feat(agent): finished ReActAgent business.

--- src/main/java/com/learning/aiagenttest/agent/ReActAgent.java
+++ src/main/java/com/learning/aiagenttest/agent/ReActAgent.java
@@ -1,4 +1,36 @@
 package com.learning.aiagenttest.agent;
 
-public class ReActAgent {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class ReActAgent extends BaseAgent {
+
+
+    private static final Logger log = LoggerFactory.getLogger(ReActAgent.class);
+
+    /**
+     * process current state and determine next step
+     * @return should act
+     */
+    public abstract boolean think();
+
+    /**
+     * execute determined action
+     * @return
+     */
+    public abstract String act();
+
+    @Override
+    public String step() {
+        try {
+            boolean shouldAct = this.think();
+            if (!shouldAct) {
+                return "Thinking finished - No action.";
+            }
+            return this.act();
+        } catch (Exception e) {
+            log.error("Execute error", e);
+            return "Step executed error: " + e.getMessage();
+        }
+    }
 }
